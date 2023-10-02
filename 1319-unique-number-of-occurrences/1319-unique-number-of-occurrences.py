29976a40305ac62c8fc53583173d322836b98a4d
Skyrockets <71587293+Astro-Yu@users.noreply.github.com>
2023-10-03 03:02:27 +0900

    Time: 47 ms (37.13%), Space: 16.4 MB (41.2%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,16 +1,14 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-
-        new_list = list(set(arr))
-        count = []
-
-        for i in range(len(new_list)):
-            new_count = arr.count(new_list[i])
-            count.append(new_count)
-
-        if len(list(set(count))) == len(count):
-            return True
-        else:
-            return False
-
+        dic = {}
         
+        # create a hash map
+        for i in arr:
+            if i in dic:
+                dic[i] += 1
+            else:
+                dic[i] = 1
+        
+        if len(set(dic.values())) != len(set(arr)):
+            return False
+        return True
