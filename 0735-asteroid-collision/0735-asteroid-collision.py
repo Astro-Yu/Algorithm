8737d1fd50a9fdd614443dddba1feadffe6072b8
Skyrockets <71587293+Astro-Yu@users.noreply.github.com>
2023-10-04 20:35:30 +0900

    Time: 82 ms (97.18%), Space: 17.4 MB (87.85%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,35 +1,10 @@
 class Solution:
-    def asteroidCollision(self, asteroids: list) -> list:
-
-        man_idx = 0
-
-        while man_idx < len(asteroids):
-            if asteroids[man_idx] > 0:
-                if man_idx == len(asteroids)-1:
-                    break
-                elif asteroids[man_idx+1] + asteroids[man_idx] > 0 and asteroids[man_idx+1] < 0:
-                    asteroids.pop(man_idx+1)
-                elif asteroids[man_idx+1] + asteroids[man_idx] == 0:
-                    asteroids.pop(man_idx)
-                    asteroids.pop(man_idx)
-                elif asteroids[man_idx+1] + asteroids[man_idx] < 0:
-                    asteroids.pop(man_idx)
-                else:
-                    man_idx += 1
-            elif asteroids[man_idx] < 0:
-                if man_idx == 0:
-                    man_idx += 1
-                elif asteroids[man_idx-1] + asteroids[man_idx] > 0:
-                    asteroids.pop(man_idx)
-                elif asteroids[man_idx-1] + asteroids[man_idx] == 0:
-                    asteroids.pop(man_idx-1)
-                    asteroids.pop(man_idx-1)
-                    man_idx -= 1
-                elif asteroids[man_idx-1] + asteroids[man_idx] < 0 and asteroids[man_idx-1] > 0:
-                    asteroids.pop(man_idx-1)
-                    man_idx -= 1
-                else:
-                    man_idx += 1
-
-            
-        return asteroids
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        s = []
+        for a in asteroids:
+            while s and s[-1] > 0 and a < 0:
+                if s[-1] + a < 0: s.pop()
+                elif s[-1] + a > 0: break    
+                else: s.pop(); break
+            else: s.append(a)        
+        return s
