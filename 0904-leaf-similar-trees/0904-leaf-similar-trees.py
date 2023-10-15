6c79965c2110e2899bff486a56b09b13d14290ba
Skyrockets <71587293+Astro-Yu@users.noreply.github.com>
2023-10-15 20:38:22 +0900

    Time: 36 ms (79.35%), Space: 16.3 MB (77.55%) - LeetHub

--- 0904-leaf-similar-trees/0904-leaf-similar-trees.py
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.py
@@ -8,9 +8,9 @@ class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         
             def rootCheck(root):
-                if not root: return [] # root가 비어있지 않다면 빈 리스트를 반환
+                if not root: return [] # root가 비어있다면 리스트를 반환
 
-                if not root.left and not root.right: # root의 왼쪽과 오른쪽가지 모두 비어있지 않다면
+                if not root.left and not root.right: # root의 왼쪽과 오른쪽가지 모두 비어있다면
                     return [root.val] # root.val 을 반환
 
                 return rootCheck(root.left) + rootCheck(root.right) # 왼쪽과 오른쪽 다 더해서 비교
